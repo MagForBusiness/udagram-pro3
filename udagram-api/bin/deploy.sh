fe8cf3b4cbb4b91ba6285c2ae8a08d149f5b648b
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-04-06 19:50:43 +0200

    55

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,4 @@
 eb init udagram-api --platform node.js --region us-east-1 &&
-eb create udagram-api-dev &&
 eb use udagram-api-dev &&
 eb setenv DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL &&
 eb deploy udagram-api-dev

72def4cfc96613c188001e05a90e5868fba1dd2d
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-04-08 00:50:07 +0200

    44

--- udagram-api/bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-eb init udagram-api --platform node.js --region us-east-1 && 
-eb use  Udagramapi-env && eb setenv DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME && 
-eb deploy  Udagramapi-env
